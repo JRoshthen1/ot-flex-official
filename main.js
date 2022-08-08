f34ccbd0be811da6b10faccea7e156ca987d7cd6
Martin Jaros <jroshthen1@gmail.com>
2022-08-09 01:27:33 +0200

    path and small changes

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-document.getElementById("navbarButton").onclick = function() {toggleNavMenu()};
+//Open and close navmenu
+document.getElementById("navbarButton",).onclick = function() {toggleNavMenu()};
+function toggleNavMenu() {
+  document.getElementById("navbarMenu").classList.toggle("openNavbarMenu");
+}
+//Close navmenu by clicking on bg
+document.getElementById("navbarMenu").onclick = function() {toggleNavMenu()};
 function toggleNavMenu() {
   document.getElementById("navbarMenu").classList.toggle("openNavbarMenu");
 }
