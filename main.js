49b137880417afe13b389823578a899571d12531
Martin Jaros <jroshthen1@gmail.com>
2022-08-11 00:27:25 +0200

    styling hero and music

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 //Open and close navmenu
-document.getElementById("navbarButton",).onclick = function() {toggleNavMenu()};
+document.getElementById("navbarButton").onclick = function() {toggleNavMenu()};
+
 function toggleNavMenu() {
   document.getElementById("navbarMenu").classList.toggle("openNavbarMenu");
+  document.getElementById("burger1").classList.toggle("burgerLine1");
+  document.getElementById("burger2").classList.toggle("burgerLine2");
+  document.getElementById("burger3").classList.toggle("burgerLine3");
 }
 //Close navmenu by clicking on bg
 document.getElementById("navbarMenu").onclick = function() {toggleNavMenu()};
-function toggleNavMenu() {
-  document.getElementById("navbarMenu").classList.toggle("openNavbarMenu");
-}
